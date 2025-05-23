a45dc573c6810e494956d627b3fcf1e2262adf7f
smmigrino <smmigrino@student.apc.edu.ph>
2025-05-23 15:39:15 +0800

    added a scaffold in home.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Welcome to the Farm Inventory App</h1>
+    <p>Select your profile to continue</p>
+  </div>
+</template>
+
+<script setup>
+// We’ll fetch farmer profiles from Supabase here later
+</script>
