2556b27a68264fafb69b9906ede6f192f3b9510f
smmigrino <smmigrino@student.apc.edu.ph>
2025-05-23 14:00:44 +0800

    added supabase client js. added .env and updated the app vue to sample

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { supabase } from '../utils/supabase'
+  const todos = ref([])
+
+  async function getTodos() {
+    const { data } = await supabase.from('todos').select()
+    todos.value = data
+  }
+
+  onMounted(() => {
+    getTodos()
+  })
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
+  </ul>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
