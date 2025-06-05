58f8f5e44116f423e99f4f35287fb1324ca2b183
smmigrino <smmigrino@student.apc.edu.ph>
2025-06-05 15:44:23 +0800

    corrected the filename

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import Inventory from '../pages/Inventory.vue';
 import ItemSelection from '../pages/ItemSelection.vue';
-import QuantityInputPage from '../pages/QuantityInputPage.vue';
+import QuantityInputPage from '../pages/QuantityInput.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
